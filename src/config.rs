787dace4fcbc87b8f547b2c3b37936bbbd4710d3
W610 <52133209+W610@users.noreply.github.com>
2026-03-30 12:10:20 +0800

    Update config.rs

--- src/config.rs
+++ src/config.rs
@@ -156,8 +156,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["113.140.8.190"];
+pub const RS_PUB_KEY: &str = "C83rKtOHvr0Jx+9msmRyO0MgOxMGWo8Vxvz+YVjD4vI=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
